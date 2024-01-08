230ebf1034cb19d471524b639716ec6482d68eab
Matthias Geier <Matthias.Geier@gmail.com>
2024-01-08 13:21:23 +0100

    Disable auto-formatting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- globally disable auto-formatting
+vim.g.autoformat = false
+
 local opt = vim.opt
 
 opt.number = false
