2fbc70f83f23ba0e7c6eed75d3610b9d794db367
Matthias Geier <Matthias.Geier@gmail.com>
2024-01-31 17:53:37 +0100

    disable more things

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,4 +1,10 @@
 return {
-  { "folke/tokyonight.nvim", enabled = false },
   { "catppuccin/nvim", enabled = false },
+  { "echasnovski/mini.indentscope", enabled = false },
+  { "echasnovski/mini.pairs", enabled = false },
+  { "folke/tokyonight.nvim", enabled = false },
+  { "lewis6991/gitsigns.nvim", enabled = false },
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
+  { "nvim-treesitter/nvim-treesitter-context", enabled = false },
+  { "RRethy/vim-illuminate", enabled = false },
 }
