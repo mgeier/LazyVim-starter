190943ab225e2a7992f93b92896fccc6577995af
Matthias Geier <Matthias.Geier@gmail.com>
2025-10-21 13:08:50 +0200

    lazy sync

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,7 +1,7 @@
 return {
   { "catppuccin/nvim", enabled = false },
-  { "echasnovski/mini.indentscope", enabled = false },
-  { "echasnovski/mini.pairs", enabled = false },
+  { "nvim-mini/mini.indentscope", enabled = false },
+  { "nvim-mini/mini.pairs", enabled = false },
   { "folke/flash.nvim", enabled = false },
   { "folke/noice.nvim", enabled = false },
   { "folke/tokyonight.nvim", enabled = false },
